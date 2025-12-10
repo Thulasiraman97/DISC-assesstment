835658c36428ab5f81eff26fecdf85b7f4b13dcd
Thulasiraman97 <133104110+Thulasiraman97@users.noreply.github.com>
2025-12-10 21:42:58 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://d6277f581bf4.ngrok-free.app/api';
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const api = axios.create({
     baseURL: API_URL,
